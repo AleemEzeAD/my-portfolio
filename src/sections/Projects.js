847ede1c35c787fbd6db2a023ae99693e9a45c26
Aleem Abbas <ALeemAbbas786>
2026-02-06 12:08:37 +0500

    fleximart added in portfolio

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -5,6 +5,37 @@ import proj2 from "../assets/images/projects/linqbuzz.webp";
 import proj3 from "../assets/images/projects/cravestay.webp";
 import proj4 from "../assets/images/projects/ezead.webp";
 
+const projects = [
+    {
+        img: proj4,
+        alt: "Ezead Classified",
+        date: "September 6, 2025",
+        title: "Ezead and Auction Site for Buying & Selling",
+        link: "https://www.ezead.com/"
+    },
+    {
+        img: proj1,
+        alt: "Chirolift",
+        date: "April 27, 2024",
+        title: "Chirolift - Physiotherapy & Chiropractic",
+        link: "https://themeforest.net/item/chirolift-physiotherapy-chiropractic-html5-website/54306022"
+    },
+    {
+        img: proj2,
+        alt: "LINQBUZZ",
+        date: "May 27, 2024",
+        title: "LINQBUZZ - Blog, Magazine & Personal Blog",
+        link: "https://themeforest.net/item/linqbuzz-blog-magazine-html5-template/54374892"
+    },
+    {
+        img: proj3,
+        alt: "Cravestay",
+        date: "September 27, 2024",
+        title: "Cravestay - Luxury Hotel & Resort",
+        link: "https://themeforest.net/item/cravestay-luxury-hotel-resort-html5-template/54976894"
+    }
+];
+
 function ProjectsSection() {
     return (
         <section className="Projects" id="projects">
@@ -18,101 +49,27 @@ function ProjectsSection() {
                 </div>
 
                 <Row>
-                    <Col xs={6} sm={6} md={6} lg={3}>
-                        <div className="proj h-100 d-flex flex-column gap-2">
-                            <figure>
-                                <img width="" height="" src={proj4} alt="Ezead classified " loading="lazy" />
-                            </figure>
-                            <div className="d-flex align-items-center gap-md-3 gap-1 icons_P">
-                                <i className="fa-regular fa-calendar"></i>
-                                <span className="date">September 6, 2025</span>
-                            </div>
-                            <div className="d-flex flex-column gap-md-3 gap-2 flex-grow-1">
-                                <h3>Ezead and Auction Site for Buying & Selling</h3>
-                                <div>
-                                    <a
-                                        href="https://www.ezead.com/"
-                                        target="_blank"
-                                        rel="noreferrer"
-                                    >
-                                        View Project <i className="fa-solid fa-arrow-right"></i>
-                                    </a>
+                    {projects.map((project, index) => (
+                        <Col key={index} xs={6} sm={6} md={6} lg={3}>
+                            <div className="proj h-100 d-flex flex-column gap-2">
+                                <figure>
+                                    <img src={project.img} alt={project.alt} loading="lazy" />
+                                </figure>
+                                <div className="d-flex align-items-center gap-md-3 gap-1 icons_P">
+                                    <i className="fa-regular fa-calendar"></i>
+                                    <span className="date">{project.date}</span>
+                                </div>
+                                <div className="d-flex flex-column gap-md-3 gap-2 flex-grow-1">
+                                    <h3>{project.title}</h3>
+                                    <div>
+                                        <a href={project.link} target="_blank" rel="noreferrer">
+                                            View Project <i className="fa-solid fa-arrow-right"></i>
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </Col>
-
-                    <Col xs={6} sm={6} md={6} lg={3}>
-                        <div className="proj h-100 d-flex flex-column gap-2">
-                            <figure>
-                                <img width="" height="" src={proj1} alt="Chirolift " loading="lazy" />
-                            </figure>
-                            <div className="d-flex align-items-center gap-md-3 gap-1 icons_P">
-                                <i className="fa-regular fa-calendar"></i>
-                                <span className="date">April 27, 2024</span>
-                            </div>
-                            <div className="d-flex flex-column gap-md-3 gap-2 flex-grow-1">
-                                <h3>Chirolift - Physiotherapy & Chiropractic</h3>
-                                <div>
-                                    <a
-                                        href="https://themeforest.net/item/chirolift-physiotherapy-chiropractic-html5-website/54306022"
-                                        target="_blank"
-                                        rel="noreferrer"
-                                    >
-                                        View Project <i className="fa-solid fa-arrow-right"></i>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </Col>
-
-                    <Col xs={6} sm={6} md={6} lg={3}>
-                        <div className="proj h-100 d-flex flex-column gap-2">
-                            <figure>
-                                <img width="" height="" src={proj2} alt="LINQBUZZ " loading="lazy" />
-                            </figure>
-                            <div className="d-flex align-items-center gap-md-3 gap-1 icons_P">
-                                <i className="fa-regular fa-calendar"></i>
-                                <span className="date">May 27, 2024</span>
-                            </div>
-                            <div className="d-flex flex-column gap-md-3 gap-2 flex-grow-1">
-                                <h3>LINQBUZZ - Blog, Magazine & Personal Blog</h3>
-                                <div>
-                                    <a
-                                        href="https://themeforest.net/item/linqbuzz-blog-magazine-html5-template/54374892"
-                                        target="_blank"
-                                        rel="noreferrer"
-                                    >
-                                        View Project <i className="fa-solid fa-arrow-right"></i>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </Col>
-
-                    <Col xs={6} sm={6} md={6} lg={3}>
-                        <div className="proj h-100 d-flex flex-column gap-2">
-                            <figure>
-                                <img width="" height="" src={proj3} alt="Cravestay " loading="lazy" />
-                            </figure>
-                            <div className="d-flex align-items-center gap-md-3 gap-1 icons_P">
-                                <i className="fa-regular fa-calendar"></i>
-                                <span className="date">September 27, 2024</span>
-                            </div>
-                            <div className="d-flex flex-column gap-md-3 gap-2 flex-grow-1">
-                                <h3>Cravestay - Luxury Hotel & Resort</h3>
-                                <div>
-                                    <a
-                                        href="https://themeforest.net/item/cravestay-luxury-hotel-resort-html5-template/54976894"
-                                        target="_blank"
-                                        rel="noreferrer"
-                                    >
-                                        View Project <i className="fa-solid fa-arrow-right"></i>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </Col>
+                        </Col>
+                    ))}
                 </Row>
             </Container>
         </section>
--- src/sections/ReactPro.js
+++ src/sections/ReactPro.js
@@ -1,10 +1,39 @@
 import { Container, Row, Col } from "react-bootstrap";
-
-// 🔹 Import images
 import proj1 from "../assets/images/react/fresh.webp";
 import proj2 from "../assets/images/react/transpo.webp";
 import proj3 from "../assets/images/react/bitbyte.webp";
-import proj4 from "../assets/images/react/portfolio.webp";
+import proj4 from "../assets/images/react/fleximart.webp";
+
+const projects = [
+    {
+        img: proj4,
+        alt: "FlexiMart",
+        date: "Jun 20, 2025",
+        title: "FlexiMart – Your Smart & Flexible Online Marketplace",
+        link: "https://aleemezead.github.io/fleximart/"
+    },
+    {
+        img: proj1,
+        alt: "Farm Fresh Hub",
+        date: "July 8, 2025",
+        title: "Farm Fresh Hub – Eat Fresh, Live Fresh",
+        link: "https://aleemezead.github.io/fresh/"
+    },
+    {
+        img: proj2,
+        alt: "Transpo-Hub",
+        date: "July 30, 2025",
+        title: "Transpo-Hub – Get your luggage safely",
+        link: "https://aleemezead.github.io/TranspoHub/"
+    },
+    {
+        img: proj3,
+        alt: "BitByte Technologies",
+        date: "August 22, 2025",
+        title: "BitByte Technologies – IT Solutions",
+        link: "https://bitbytetechnologies.com/"
+    }
+];
 
 function ReactProjects() {
     return (
@@ -19,101 +48,27 @@ function ReactProjects() {
                     </p>
                 </div>
                 <Row>
-                    <Col xs={6} sm={6} md={6} lg={3}>
-                        <div className="proj h-100 d-flex flex-column gap-2">
-                            <figure>
-                                <img width="" height="" src={proj4} alt="Cravestay" loading="lazy" />
-                            </figure>
-                            <div className="d-flex align-items-center gap-md-3 gap-2 icons_P">
-                                <i className="fa-regular fa-calendar"></i>
-                                <span className="date">Jun 20, 2025</span>
-                            </div>
-                            <div className="d-flex flex-column gap-md-3 gap-2 flex-grow-1">
-                                <h3>Aleem FullStack Web Developer Portfolio</h3>
-                                <div>
-                                    <a
-                                        href="https://aleemezead.github.io/my-portfolio/"
-                                        target="_blank"
-                                        rel="noreferrer"
-                                    >
-                                        View Project <i className="fa-solid fa-arrow-right"></i>
-                                    </a>
+                    {projects.map((project, index) => (
+                        <Col key={index} xs={6} sm={6} md={6} lg={3}>
+                            <div className="proj h-100 d-flex flex-column gap-2">
+                                <figure>
+                                    <img src={project.img} alt={project.alt} loading="lazy" />
+                                </figure>
+                                <div className="d-flex align-items-center gap-md-3 gap-2 icons_P">
+                                    <i className="fa-regular fa-calendar"></i>
+                                    <span className="date">{project.date}</span>
+                                </div>
+                                <div className="d-flex flex-column gap-md-3 gap-2 flex-grow-1">
+                                    <h3>{project.title}</h3>
+                                    <div>
+                                        <a href={project.link} target="_blank" rel="noreferrer">
+                                            View Project <i className="fa-solid fa-arrow-right"></i>
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </Col>
-                    
-                    <Col xs={6} sm={6} md={6} lg={3}>
-                        <div className="proj h-100 d-flex flex-column gap-2">
-                            <figure>
-                                <img width="" height="" src={proj1} alt="Cravestay" loading="lazy" />
-                            </figure>
-                            <div className="d-flex align-items-center gap-md-3 gap-2 icons_P">
-                                <i className="fa-regular fa-calendar"></i>
-                                <span className="date">July 8, 2025</span>
-                            </div>
-                            <div className="d-flex flex-column gap-md-3 gap-2 flex-grow-1">
-                                <h3>Farm Fresh Hub – Eat Fresh, Live Fresh</h3>
-                                <div>
-                                    <a
-                                        href="https://aleemezead.github.io/fresh/"
-                                        target="_blank"
-                                        rel="noreferrer"
-                                    >
-                                        View Project <i className="fa-solid fa-arrow-right"></i>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </Col>
-
-                    <Col xs={6} sm={6} md={6} lg={3}>
-                        <div className="proj h-100 d-flex flex-column gap-2">
-                            <figure>
-                                <img width="" height="" src={proj2} alt="LINQBUZZ" loading="lazy" />
-                            </figure>
-                            <div className="d-flex align-items-center gap-md-3 gap-2 icons_P">
-                                <i className="fa-regular fa-calendar"></i>
-                                <span className="date">July 30, 2025</span>
-                            </div>
-                            <div className="d-flex flex-column gap-md-3 gap-2 flex-grow-1">
-                                <h3>Transpo-Hub Get you luggage safely</h3>
-                                <div>
-                                    <a
-                                        href="https://aleemezead.github.io/TranspoHub/"
-                                        target="_blank"
-                                        rel="noreferrer"
-                                    >
-                                        View Project <i className="fa-solid fa-arrow-right"></i>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </Col>
-
-                    <Col xs={6} sm={6} md={6} lg={3}>
-                        <div className="proj h-100 d-flex flex-column gap-2">
-                            <figure>
-                                <img width="" height="" src={proj3} alt="Caring Crest" loading="lazy" />
-                            </figure>
-                            <div className="d-flex align-items-center gap-md-3 gap-2 icons_P">
-                                <i className="fa-regular fa-calendar"></i>
-                                <span className="date">August 22, 2025</span>
-                            </div>
-                            <div className="d-flex flex-column gap-md-3 gap-2 flex-grow-1">
-                                <h3>BitByte Technologies IT Solutions</h3>
-                                <div>
-                                    <a
-                                        href="https://bitbytetechnologies.com/"
-                                        target="_blank"
-                                        rel="noreferrer"
-                                    >
-                                        View Project <i className="fa-solid fa-arrow-right"></i>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </Col>
+                        </Col>
+                    ))}
                 </Row>
             </Container>
         </section>
